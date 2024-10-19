0a14dc9ac86f582a2b27988c5f5a26c0a4ab2116
AEKDA <k.david.doc@gmail.com>
2024-10-19 19:54:49 +0300

    [master] fix sql scripts

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,11 @@
+-- Active: 1695848398724@@127.0.0.1@5433@is_work
 create table users (
     id serial PRIMARY KEY,
     login TEXT not null UNIQUE,
     passwd TEXT not null
 );
 
-CREATE Table customer (
+CREATE Table customers (
     id serial PRIMARY KEY,
     user_id INTEGER not null unique REFERENCES users,
     email text not null unique,
@@ -38,53 +39,48 @@ create table clinics (
     user_id INTEGER not null unique REFERENCES users
 );
 
-create table posts (
-    id serial PRIMARY KEY,
-    name text
-);
 
 create table specializations (
     id serial PRIMARY KEY,
     name text
 );
 
-create table doctor (
+create table doctors (
     id serial primary key,
     user_id integer not null REFERENCES users,
     description text,
     name text,
-    clinic_id int not null REFERENCES clinics, 
-    post text REFERENCES posts
+    clinic_id int not null REFERENCES clinics
 );
 
 create table specialization_doctor (
-    id_specializations int REFERENCES specializations,
-    id_doctor int REFERENCES doctor,
-    PRIMARY KEY(id_doctor,id_specializations)
+    specializations_id int REFERENCES specializations,
+    doctor_id int REFERENCES doctors,
+    PRIMARY KEY(specializations_id,doctor_id)
 );
 
 create table busy_hours (
     doctor_id int REFERENCES doctors,
-    start TIMESTAMP not null,
-    end TIMESTAMP not null,
-    check (start < end)
+    "start" TIMESTAMP not null,
+    "end" TIMESTAMP not null,
+    check ("start" < "end")
 );
 
 create table timetable (
     doctor_id int REFERENCES doctors,
     customer_id int REFERENCES customers,
-    start TIMESTAMP not null,
-    end TIMESTAMP not null,
-    check (start < end)
+    "start" TIMESTAMP not null,
+    "end" TIMESTAMP not null,
+    check ("start" < "end")
 );
 
 create table request (
     id serial PRIMARY KEY,
     clinic_id int REFERENCES clinics,
     customer_id int REFERENCES customers,
-    doctor_id int REFERENCES doctors
-    start TIMESTAMP not null,
-    end TIMESTAMP not null, 
-    CHECK (start < end),
-    CHECK (EXTRACT(EPOCH FROM end - start) > 3600)
+    doctor_id int REFERENCES doctors,
+    "start" TIMESTAMP not null,
+    "end" TIMESTAMP not null, 
+    CHECK ("start" < "end"),
+    CHECK (EXTRACT(EPOCH FROM "end" - "start") > 3600)
 );
